630ff93314cca0d7c6dcf7ce73da37d9c0b6741b
Goyeneche23 <juanjogoyeneche@gmail.com>
2025-10-14 11:50:47 -0600

    jk

--- app/src/main/java/com/example/examen/data/repository/CountryRepositoryImpl.kt
+++ app/src/main/java/com/example/examen/data/repository/CountryRepositoryImpl.kt
@@ -3,19 +3,21 @@ package com.example.examen.data.repository
 import com.example.examen.data.mapper.toDomain
 import com.example.examen.data.remote.api.CountryApi
 import com.example.examen.domain.model.Country
+import com.example.examen.domain.repository.CountryRepository
+
 import javax.inject.Inject
 
 class CountryRepositoryImpl @Inject constructor(
     private val api: CountryApi
-) {
-    suspend fun getCountries(): List<Country> =
+) : CountryRepository {
+    override suspend fun getCountries(): List<Country> =
         api.getAllCountries()
             .map { it.toDomain() }
             .sortedBy { it.name.lowercase() }
 
-    suspend fun getCountryByCode(code: String): Country? =
+    override suspend fun getCountryByCode(code: String): Country? =
         api.getCountryByCode(code).firstOrNull()?.toDomain()
 
-    suspend fun getCountryByName(name: String): List<Country> =
+    override suspend fun getCountryByName(name: String): List<Country> =
         api.getCountryByName(name).map { it.toDomain() }
 }
